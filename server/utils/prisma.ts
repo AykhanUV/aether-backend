546897b98af1b34c3beef4001f36e250d7446726
Aykhan <ayxanshikhaliyevuv@gmail.com>
2025-12-07 16:55:36 +0400

    Revert "Fix SSL error using pg Pool directly with rejectUnauthorized false"
    This reverts commit e5a8255c0e859fd770159651194f75501c70113a.

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,15 +1,12 @@
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../generated/client';
-import { Pool } from 'pg';
 
-const pool = new Pool({
+const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 });
-
-const adapter = new PrismaPg(pool);
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
